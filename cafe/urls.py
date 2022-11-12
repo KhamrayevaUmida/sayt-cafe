62a7583ab8d4f49594d1621b6bceed928347aefe
Your Name <you@example.com>
2022-11-12 23:57:46 +0500

    new

--- cafe/urls.py
+++ cafe/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
     path('fast_food/', fast_food),
+    path('pizza/', pizza),
+    path('coffee/', coffee),
+    path('drink/', drink),
+    path('help/', help),
+    path('order/', order),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,18 @@ def main(request):
 
 def fast_food(request):
     return render(request, 'fast_food.html')
+
+def pizza(request):
+    return render(request, 'pizza.html')
+
+def coffee(request):
+    return render(request, 'coffee.html')
+
+def drink(request):
+    return render(request, 'drink.html')
+
+def help(request):
+    return render(request, 'help.html')
+
+def order(request):
+    return render(request, 'order.html')
